4c8a9484316d97b01520759ec2be7f2ca254a707
Jake Saunders <jakesaunders@gmail.com>
2017-05-22 17:14:24 -0400

    Add files via upload

--- ab1Files2SequenceDF.R
+++ ab1Files2SequenceDF.R
@@ -1,5 +1,5 @@
 
-# load packages
+# load packages----
 source("https://bioconductor.org/biocLite.R")
 biocLite("sangerseqR")
 install.packages("tidyr")
@@ -16,37 +16,34 @@ library(plyr)
 # set wd
 setwd("C:/Users/saundecj/Google Drive/@R/Sanger")
 
-# clean data from sequence files names
-file.name <- list.files("seqfiles/",recursive = T)
-file.name <- file.name[grep(pattern = ".ab1",x = file.name)]
+# clean file info from sequence files names ----
 
-file.info <- sub(".ab1","",x = file.name)
-files.info <- as.data.frame(t(as.data.frame(strsplit(x = file.info, "_"))))
+library(plyr)
 
-files.info <- cbind.data.frame(
-  files.info,
-  colsplit(files.info$V3,pattern = "-",names = c("sample.name","sample.number")),
-  colsplit(files.info$V4,pattern = "-",names = c("primer","direction")),
-  file.name
-)
-rownames(files.info) <- NULL
-files.info$direction <- sub("for|1-for|3-for",replacement = "F",
-                        sub("rev|r-rev|4-rev",replacement = "R",files.info$direction)
-  )
-rm(file.info,file.name)
-write.csv(files.info, file="SangerSeqRunInfo.csv")
+generateSeqFilesInfo <- function(directory, file.type = ".ab1"){
+  file.path <- list.files(directory, recursive = T, full.names = T)
+  file.path <- file.path[grep(pattern = file.type,x = file.path)]
+  
+  file.info <- t(as.data.frame(
+    strsplit(x = sub(".ab1","",x = file.path),"_"),
+    stringsAsFactors = F
+  ))
+  file.info <- cbind.data.frame(file.path,file.info[,c(3:4)],stringsAsFactors = F)
+  
+  rownames(file.info) <- NULL
+  colnames(file.info) <- c("file.path","sample.name.full","primer.name.full")
+  file.info
+}
 
-# select degen worm pcr sanger seq files
-files.info <- files.info[43:82,6:10]
 
-##### maybe start here
-sanger.file1 <- paste0("seqfiles/",files.info$file.name)
-sanger.file2 <- paste0("seqfiles/",files.info$file.name[2])
+# extract sequences given .ab1 file ----
 
-extractSequences <- function(sanger.file){
+library(sangerseqR)
+
+extractSequences <- function(sanger.file, basecall.ratio = 0.33){
   # make seqs objects
   sanger.seq <- readsangerseq(sanger.file)
-  basecall.seq <- makeBaseCalls(sanger.seq)
+  basecall.seq <- makeBaseCalls(sanger.seq,ratio = basecall.ratio)
   # put seqs into data frame row
   row.out <- cbind.data.frame(
     as.character(sanger.file),
@@ -56,7 +53,7 @@ extractSequences <- function(sanger.file){
     stringsAsFactors = F
   )
   names(row.out) <- c(
-    "source.file",
+    "file.path",
     "seq.consensus",
     "seq.basecall.primary",
     "seq.basecall.secondary"
@@ -64,8 +61,43 @@ extractSequences <- function(sanger.file){
   print(row.out)
 }
 
-sanger.files <- paste0("seqfiles/",files.info$file.name)
-files.seq <- ldply(sanger.files,extractSequences)
+
+# make dfs and put together ----
+
+library(plyr)
+
+file.info <- generateSeqFilesInfo("Sanger/seqfiles/")  # remember file extention
+
+seqs <- ldply(
+  file.info$file.path, 
+  extractSequences, 
+  basecall.ratio = 0.33
+)
+
+test <- join(file.info,seqs,by = "file.path")
+
+save(test,file = "sequencedataframe.Rdata")
+write.csv(test,file = "sangerseqorder2017-05.csv")
+
+#### TODO: Just need to put everything in a wrapper and run on files
+
+###ideas for later
+# lenght in basepairs in data frame
+
+#################################################################
+#### save this for later, use when aligning like samples ########
+# files.info <- cbind.data.frame(
+#   files.info,
+#   colsplit(files.info$V3,pattern = "-",names = c("sample.name","sample.number")),
+#   colsplit(files.info$V4,pattern = "-",names = c("primer","direction")),
+#   file.name
+# )
+#
+#
+# files.info$direction <- sub("for|1-for|3-for",replacement = "F",
+#                             sub("rev|r-rev|4-rev",replacement = "R",files.info$direction)
+# )
+#################################################################
 
 ##### note #####
 # AlignSeqs
